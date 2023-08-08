66a54ee419d3962262e00fd39643110b9704a9f1
syxanash <syxanash@gmail.com>
2023-08-08 18:58:31 +0100

    restyled page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useCallback } from 'react';
 import { ThemeProvider } from 'styled-components';
 import {
   Window,
   WindowContent,
   ProgressBar,
-  TextInput
+  TextInput,
+  Button,
 } from 'react95';
 import windowTheme from 'react95/dist/themes/raspberry';
 
@@ -12,18 +13,22 @@ import LoaderCursor from './LoaderCursor';
 
 import styles from './page.module.css';
 
+import blackCursor from './cursor.gif';
 import logo from './logo.png';
 
 function App() {
   const [percent, setPercent] = useState(0);
+  const [unzipping, setUnzipping] = useState(false);
 
-  useEffect(() => {
+  const unzip = useCallback(() => {
+    setUnzipping(true);
+
     const timer = setInterval(() => {
       setPercent(previousPercent => {
         if (previousPercent === 100) {
           window.location.replace('https://simone.computer/#/webdesktops');
         }
-        const diff = Math.random() * 25;
+        const diff = Math.random() * 40;
         return Math.min(previousPercent + diff, 100);
       });
     }, 500);
@@ -32,33 +37,39 @@ function App() {
     };
   }, []);
 
+  const renderButtons = useCallback(() => {
+    return <div className={styles.buttonWrapper}>
+      <Button style={{ width: '100px', marginRight: '10px', cursor: `url(${blackCursor}), auto` }} size='md' onClick={() => window.location.replace('https://simone.computer')}>Cancel</Button>
+      <Button style={{ width: '100px', cursor: `url(${blackCursor}), auto` }} primary size='md' onClick={unzip}>Ok</Button>
+    </div>
+  }, [unzip]);
+
   return <>
-    <div className={styles.window}>
+    <div id="mainwindow" className={styles.window}>
       <ThemeProvider theme={windowTheme}>
-        <Window shadow={false} style={ { width: '100%' } }>
+        <Window shadow={false} style={ { width: '100%', height: '100%' } }>
           <WindowContent>
-            <div>
-              <div className={styles.headerError}>
-                <div style={{ paddingTop: '15px', paddingBottom: '15px' }}>
-                  <img src={logo} alt='logo' />
-                  <span style={{ fontSize: '27px', position: 'absolute', top: '37px', left: '100px'}}>Pippo OS Archiver</span>
-                </div>
+            <div className={styles.headerError}>
+              <div style={{ paddingTop: '15px', paddingBottom: '15px' }}>
+                <img src={logo} alt='logo' />
+                <span style={{ fontSize: '27px', position: 'absolute', top: '37px', left: '100px'}}>Pippo OS Archiver</span>
               </div>
-              <div style={{ paddingBottom: '50px', paddingTop: '20px' }}>
-                <div><h3>Unpacking <span style={{ fontStyle: 'italic' }}>desktops.zip</span> to:</h3></div>
-                <TextInput
-                  aria-label="directory"
-                  style={{ maxWidth: '300px' }}
-                  value={'//simone.computer/#/webdesktops'}
-                />
-              </div>
-              <ProgressBar aria-label="progress" variant='tile' value={Math.floor(percent)} />
             </div>
+            <div style={{ paddingTop: '20px' }}>
+              <div><h3>Unpacking <span style={{ fontStyle: 'italic' }}>desktops.zip</span> to:</h3></div>
+              <TextInput
+                aria-label="directory"
+                style={{ maxWidth: '330px' }}
+                value={'simone.computer/#/webdesktops'}
+              />
+            </div>
+            { unzipping ? <div style={{ paddingTop: '35px' }}><ProgressBar aria-label="progress" variant='tile' value={Math.floor(percent)} /></div> : null }
+            { unzipping ? null : renderButtons() }
           </WindowContent>
         </Window>
       </ThemeProvider>
     </div>
-    <LoaderCursor />
+    { unzipping ? <LoaderCursor /> : null }
   </>
 }
 
